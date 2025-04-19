cdedc7825939f3fe604fdeb316687d2947e34aa9
Manish Roy <manishroy0112@gmail.com>
2025-04-19 18:19:06 +0530

    handled test case #3 and #5

--- rotate.rb
+++ rotate.rb
@@ -1,7 +1,20 @@
 def rotate_elements(n, arr)
   return 'Cannot operate on empty array' if arr.empty?
 
-  arr
+  min_element = lambda do |a|
+    min = a[0]
+    (1...a.length).each do |i|
+      min = a[i] if a[i] < min
+    end
+    min
+  end
+
+  arr_value = arr.dup
+  while arr_value[0] != min_element.call(arr_value)
+    arr_value = arr_value[1..-1] + [arr_value[0]]
+  end
+
+  arr_value
 end
 
 # Example usage (uncomment to run directly)
